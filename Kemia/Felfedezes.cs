354634d7523ddef9344bed6219d58531115283c7
Adrián Medve <medveadrian03@gmail.com>
2022-10-26 14:28:42 +0200

    create class

---
+++ Kemia/Felfedezes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kemia
+{
+    class Felfedezes
+    {
+        public string Ev { get; set; }
+
+        public string Nev { get; set; }
+
+        public string Vegyjel { get; set; }
+
+        public int Rendszam { get; set; }
+
+        public string Felfedezo { get; set; }
+
+        public Felfedezes(string ev, string nev, string vegyjel, int rendszam, string felfedezo)
+        {
+            Ev = ev;
+            Nev = nev;
+            Vegyjel = vegyjel;
+            Rendszam = rendszam;
+            Felfedezo = felfedezo;
+        }
+    }
+}
